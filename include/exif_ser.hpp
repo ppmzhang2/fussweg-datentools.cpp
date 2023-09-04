dcb4776c88ba79651f91c5d5d0f36d24be0a62c3
ZHANG, Meng <stall-breezes.0t@icloud.com>
2023-09-04 23:39:24 +1200

    re-org

--- include/exif_ser.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "exif.hpp"
-
-namespace ExifSerializer {
-    std::string Serialize(const ExifAttrs &attrs);
-} // namespace ExifSerializer
--- src/file_iter.cpp
+++ src/file_iter.cpp
@@ -1,27 +1,29 @@
 #include "file_iter.hpp"
 
-JPEGFilesIterator::JPEGFilesIterator(const std::string &dir) {
+std::vector<std::string> jpegFiles;
+std::vector<std::string>::iterator currentIter;
+
+FileIter::FileIter(const std::string &dir) {
     for (const auto &f : std::filesystem::recursive_directory_iterator(dir)) {
         if (f.path().extension() == ".JPG" || f.path().extension() == ".jpg") {
-            jpegFiles.push_back(f.path().string());
+            this->jpegFiles.push_back(f.path().string());
         }
     }
-    currentIter = jpegFiles.begin();
+    this->currentIter = this->jpegFiles.begin();
 }
 
-std::vector<std::string>::iterator JPEGFilesIterator::begin() {
-    return jpegFiles.begin();
+std::vector<std::string>::iterator FileIter::begin() {
+    return this->jpegFiles.begin();
+}
+std::vector<std::string>::iterator FileIter::end() {
+    return this->jpegFiles.end();
 }
 
-std::vector<std::string>::iterator JPEGFilesIterator::end() {
-    return jpegFiles.end();
-}
+bool FileIter::hasNext() { return this->currentIter != this->jpegFiles.end(); }
 
-bool JPEGFilesIterator::hasNext() { return currentIter != jpegFiles.end(); }
-
-std::string JPEGFilesIterator::next() {
-    if (hasNext()) {
-        return *(currentIter++);
+std::string FileIter::next() {
+    if (this->hasNext()) {
+        return *(this->currentIter++);
     }
     throw std::out_of_range("No more JPEG files.");
 }
