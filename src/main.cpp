1b011c5ba108b1255cb831075abb835a39e67ea5
ZHANG, Meng <stall-breezes.0t@icloud.com>
2023-09-11 22:58:19 +1200

    Draft Concurrent OpticalFlow Module

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "exif.hpp"
 #include "exif_serde.hpp"
 #include "file_iter.hpp"
+#include "optical_flow.hpp"
 #include <filesystem>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 void writeToFile(const std::string &out_file, const std::string &content) {
     std::ofstream outFile(out_file);
@@ -21,17 +23,21 @@ void writeToFile(const std::string &out_file, const std::string &content) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cout << "Fussweg Datentools" << std::endl;
-        std::cout << "Version: " << VERSION_MAJOR << "." << VERSION_MINOR << "."
-                  << VERSION_PATCH << std::endl;
-        std::cout << std::endl;
-        std::cerr << "Usage: " << argv[0]
-                  << " <directory_path> <output_file_path>" << std::endl;
-        return 1;
-    }
+    // if (argc != 3) {
+    //     std::cout << "Fussweg Datentools" << std::endl;
+    //     std::cout << "Version: " << VERSION_MAJOR << "." << VERSION_MINOR <<
+    //     "."
+    //               << VERSION_PATCH << std::endl;
+    //     std::cout << std::endl;
+    //     std::cerr << "Usage: " << argv[0]
+    //               << " <directory_path> <output_file_path>" << std::endl;
+    //     return 1;
+    // }
 
     std::string dir_path = argv[1];
-    std::string out_path = argv[2];
-    writeToFile(out_path, dir_path);
+
+    nlohmann::json j = OpticalFlow::Displacement(dir_path);
+    std::cout << j << std::endl;
+    // std::string out_path = argv[2];
+    // writeToFile(out_path, dir_path);
 }
