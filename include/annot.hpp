8afa3e9552b9e580b9b94b30400cdd9be30900dc
ppmzhang2 <50707839+ppmzhang2@users.noreply.github.com>
2023-12-13 02:07:41 +1300

    Revise Fault struct of Box (#16)

--- include/annot.hpp
+++ include/annot.hpp
@@ -2,14 +2,68 @@
 
 #include <iostream>
 
+struct Fault {
+    unsigned int bump : 2;
+    unsigned int crack : 2;
+    unsigned int depression : 2;
+    unsigned int displacement : 2;
+    unsigned int vegetation : 2;
+    unsigned int uneven : 2;
+    unsigned int pothole : 2;
+    unsigned int padding : 2;
+
+    // Constructor
+    Fault()
+        : bump(0), crack(0), depression(0), displacement(0), vegetation(0),
+          uneven(0), pothole(0), padding(0) {}
+
+    std::string ToStr() const {
+        const std::string faults[7] = {
+            fault_to_str(bump, "bump"),
+            fault_to_str(crack, "crack"),
+            fault_to_str(depression, "depression"),
+            fault_to_str(displacement, "displacement"),
+            fault_to_str(vegetation, "vegetation"),
+            fault_to_str(uneven, "uneven"),
+            fault_to_str(pothole, "pothole"),
+
+        };
+        std::string res = "";
+        for (const auto &f : faults) {
+            if (!f.empty() && res.empty()) {
+                res = f;
+            } else if (!f.empty()) {
+                res += "_" + f;
+            } else {
+                continue;
+            }
+        }
+        return res;
+    }
+
+  private:
+    std::string fault_to_str(unsigned int cond,
+                             const std::string &fault) const {
+        switch (cond) {
+        case 1:
+            return fault + "_fair";
+        case 2:
+            return fault + "_poor";
+        case 3:
+            return fault + "_verypoor";
+        default:
+            return "";
+        }
+    }
+};
+
 struct Box {
     int x;
     int y;
     int w;
     int h;
     std::string image;
-    std::array<bool, 7> faults;
-    std::array<bool, 3> conds;
+    Fault fault;
 };
 
 struct BBox {
@@ -18,22 +72,6 @@ struct BBox {
 };
 
 namespace Annot {
-
-    static const std::array<std::string, 7> kFaultNames = {
-        "bump",       "crack",  "depression", "displacement",
-        "vegetation", "uneven", "pothole"};
-
-    static const std::array<std::string, 3> kCondNames = {
-        "fair",
-        "poor",
-        "verypoor",
-    };
-
-    // map Fault to string
-    std::vector<std::string> FaultToStr(const Box &);
-
-    // map Condition to string
-    std::vector<std::string> CondToStr(const Box &);
 
     void PrintBBox(const std::vector<BBox> &);
 
