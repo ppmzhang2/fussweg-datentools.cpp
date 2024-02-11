71e236ba8d5c79e91279d3d1e2d3e3b8ec111174
ZHANG, Meng <stall-breezes.0t@icloud.com>
2024-02-11 15:06:05 +1300

    bbox-stat

--- include/annot.hpp
+++ include/annot.hpp
@@ -71,6 +71,89 @@ struct BBox {
     std::vector<Box> boxes;
 };
 
+struct FaultStats {
+    unsigned int bump_fair;
+    unsigned int bump_poor;
+    unsigned int bump_verypoor;
+    unsigned int crack_fair;
+    unsigned int crack_poor;
+    unsigned int crack_verypoor;
+    unsigned int depression_fair;
+    unsigned int depression_poor;
+    unsigned int depression_verypoor;
+    unsigned int displacement_fair;
+    unsigned int displacement_poor;
+    unsigned int displacement_verypoor;
+    unsigned int vegetation_fair;
+    unsigned int vegetation_poor;
+    unsigned int vegetation_verypoor;
+    unsigned int uneven_fair;
+    unsigned int uneven_poor;
+    unsigned int uneven_verypoor;
+    unsigned int pothole_fair;
+    unsigned int pothole_poor;
+    unsigned int pothole_verypoor;
+
+    // constructor
+    FaultStats()
+        : bump_fair(0), bump_poor(0), bump_verypoor(0), crack_fair(0),
+          crack_poor(0), crack_verypoor(0), depression_fair(0),
+          depression_poor(0), depression_verypoor(0), displacement_fair(0),
+          displacement_poor(0), displacement_verypoor(0), vegetation_fair(0),
+          vegetation_poor(0), vegetation_verypoor(0), uneven_fair(0),
+          uneven_poor(0), uneven_verypoor(0), pothole_fair(0), pothole_poor(0),
+          pothole_verypoor(0) {}
+
+    // add fault
+    void AddFault(const Fault &f) {
+        bump_fair += f.bump == 1;
+        bump_poor += f.bump == 2;
+        bump_verypoor += f.bump == 3;
+        crack_fair += f.crack == 1;
+        crack_poor += f.crack == 2;
+        crack_verypoor += f.crack == 3;
+        depression_fair += f.depression == 1;
+        depression_poor += f.depression == 2;
+        depression_verypoor += f.depression == 3;
+        displacement_fair += f.displacement == 1;
+        displacement_poor += f.displacement == 2;
+        displacement_verypoor += f.displacement == 3;
+        vegetation_fair += f.vegetation == 1;
+        vegetation_poor += f.vegetation == 2;
+        vegetation_verypoor += f.vegetation == 3;
+        uneven_fair += f.uneven == 1;
+        uneven_poor += f.uneven == 2;
+        uneven_verypoor += f.uneven == 3;
+        pothole_fair += f.pothole == 1;
+        pothole_poor += f.pothole == 2;
+        pothole_verypoor += f.pothole == 3;
+    }
+
+    void Print() const {
+        std::cout << "bump_fair: " << bump_fair << std::endl;
+        std::cout << "bump_poor: " << bump_poor << std::endl;
+        std::cout << "bump_verypoor: " << bump_verypoor << std::endl;
+        std::cout << "crack_fair: " << crack_fair << std::endl;
+        std::cout << "crack_poor: " << crack_poor << std::endl;
+        std::cout << "crack_verypoor: " << crack_verypoor << std::endl;
+        std::cout << "depression_fair: " << depression_fair << std::endl;
+        std::cout << "depression_poor: " << depression_poor << std::endl;
+        std::cout << "depression_verypoor: " << depression_verypoor << std::endl;
+        std::cout << "displacement_fair: " << displacement_fair << std::endl;
+        std::cout << "displacement_poor: " << displacement_poor << std::endl;
+        std::cout << "displacement_verypoor: " << displacement_verypoor << std::endl;
+        std::cout << "vegetation_fair: " << vegetation_fair << std::endl;
+        std::cout << "vegetation_poor: " << vegetation_poor << std::endl;
+        std::cout << "vegetation_verypoor: " << vegetation_verypoor << std::endl;
+        std::cout << "uneven_fair: " << uneven_fair << std::endl;
+        std::cout << "uneven_poor: " << uneven_poor << std::endl;
+        std::cout << "uneven_verypoor: " << uneven_verypoor << std::endl;
+        std::cout << "pothole_fair: " << pothole_fair << std::endl;
+        std::cout << "pothole_poor: " << pothole_poor << std::endl;
+        std::cout << "pothole_verypoor: " << pothole_verypoor << std::endl;
+    }
+};
+
 namespace Annot {
 
     void PrintBBox(const std::vector<BBox> &);
@@ -80,5 +163,7 @@ namespace Annot {
     void ImgWrite(const BBox &, const std::string &, const std::string &);
 
     void VisBBox(const std::string &, const std::string &, const std::string &);
+
+    void Stats(const std::string &);
 
 } // namespace Annot
