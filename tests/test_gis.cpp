28ecf80ccf0a170771ce468b5c9c0aa067935e49
Juan Cervantes <stall-breezes.0t@icloud.com>
2024-12-02 00:05:20 +1300

    geojson-to-tsv: add integer ID and surface material

--- tests/test_gis.cpp
+++ tests/test_gis.cpp
@@ -31,7 +31,7 @@ static constexpr char kGeoJson[] = R"({
                 "width": 2.6,
                 "area": 475.8,
                 "total_area": 475.8,
-                "footpath_surf_mat": "Asphaltic concrete black",
+                "footpath_surf_mat": "Asphaltic concrete black 1",
                 "age": 15,
                 "purpose": "Both",
                 "pedestrian_use": "Medium",
@@ -71,7 +71,7 @@ static constexpr char kGeoJson[] = R"({
                 "width": 1.5,
                 "area": 63.0,
                 "total_area": 63.0,
-                "footpath_surf_mat": "Asphaltic concrete black",
+                "footpath_surf_mat": "Asphaltic concrete black 2",
                 "age": 44,
                 "purpose": "Footpath",
                 "pedestrian_use": null,
@@ -120,7 +120,7 @@ static constexpr char kGeoJson[] = R"({
                 "width": 1.4,
                 "area": 88.2,
                 "total_area": 88.2,
-                "footpath_surf_mat": "Asphaltic concrete black",
+                "footpath_surf_mat": "Asphaltic concrete black 3",
                 "age": 17,
                 "purpose": "Footpath",
                 "pedestrian_use": "Low/medium",
@@ -174,7 +174,7 @@ static constexpr char kGeoJson[] = R"({
                 "width": 4.0,
                 "area": 680.0,
                 "total_area": 680.0,
-                "footpath_surf_mat": "Asphaltic concrete black",
+                "footpath_surf_mat": "Asphaltic concrete black 4",
                 "age": 2,
                 "purpose": "Both",
                 "pedestrian_use": "Low/medium",
@@ -291,7 +291,7 @@ static constexpr char kGeoJson[] = R"({
                 "width": 1.5,
                 "area": 381.0,
                 "total_area": 381.0,
-                "footpath_surf_mat": "Asphaltic concrete black",
+                "footpath_surf_mat": "Asphaltic concrete black 5",
                 "age": 44,
                 "purpose": "Footpath",
                 "pedestrian_use": "Low/medium",
@@ -383,31 +383,39 @@ static constexpr char kWktMpolygon[] =
      "5420787.619000, 1741831.291000 5420817.648000, 1741786.274000 "
      "5420817.648000, 1741786.274000 5420787.619000)))");
 static constexpr char kWktCsv[] =
-    "id\troad\tside\tstart\tclose\tlength\twidth\tarea\tage\twkt\nfc6e0039-"
-    "06ad-43f6-94cb-c703edfb63b8\tBLENHEIM RD1/ACCESS TO RLY "
-    "LINE\tUnknown\t170\t0\t183\t0\t475.8\t15\tPOINT (1568455.878400 "
-    "5179521.088200)\nfb9baee1-1d85-442d-b691-48fddf7ad861\tCOURTENAY "
-    "ST\tRight\t190\t215\t42\t0\t63\t44\tLINESTRING (1570453.177587 "
+    "entity_id\tasset_"
+    "id\troad\tside\tstart\tclose\tlength\twidth\tarea\tage\tmaterial\twkt\n"
+    "fc6e0039-06ad-43f6-94cb-c703edfb63b8\t24143\tBLENHEIM RD1/ACCESS TO RLY "
+    "LINE\tUnknown\t170\t0\t183\t0\t475.8\t15\tAsphaltic concrete black "
+    "1\tPOINT (1568455.878400 "
+    "5179521.088200)\nfb9baee1-1d85-442d-b691-48fddf7ad861\t27733\tCOURTENAY "
+    "ST\tRight\t190\t215\t42\t0\t63\t44\tAsphaltic concrete black "
+    "2\tLINESTRING "
+    "(1570453.177587 "
     "5182792.085639, 1570463.685564 5182793.860214, 1570471.708698 "
     "5182793.679381, 1570479.612996 5182795.874611, 1570483.571308 "
     "5182795.567898, 1570490.786123 5182794.303225, 1570492.245839 "
     "5182795.173844, 1570493.481633 "
-    "5182797.663887)\n7c0db807-1d97-4a2a-880a-d6faa32ecfac\tVenilla "
-    "Polygon\tRight\t4\t67\t63\t0\t88.2\t17\tPOLYGON ((1568287.246207 "
+    "5182797.663887)\n7c0db807-1d97-4a2a-880a-d6faa32ecfac\t23295\tVenilla "
+    "Polygon\tRight\t4\t67\t63\t0\t88.2\t17\tAsphaltic concrete black "
+    "3\tPOLYGON ((1568287.246207 "
     "5179502.694713, 1568291.926131 5179501.564891, 1568299.977034 "
     "5179500.490728, 1568305.678037 5179500.717903, 1568356.622853 "
     "5179506.519054, 1568356.781250 5179505.128043, 1568305.785251 "
     "5179499.321064, 1568299.911847 5179499.087020, 1568291.668584 "
     "5179500.186847, 1568286.917660 5179501.333810, 1568287.246207 "
-    "5179502.694713))\n33f00889-b86e-4300-a069-428f1ae78531\tPolygon with "
-    "One Hole\tLeft\t1\t173\t170\t0\t680\t2\tPOLYGON ((1740568.898000 "
+    "5179502.694713))\n33f00889-b86e-4300-a069-428f1ae78531\t28434\tPolygon "
+    "with "
+    "One Hole\tLeft\t1\t173\t170\t0\t680\t2\tAsphaltic concrete black "
+    "4\tPOLYGON ((1740568.898000 "
     "5419812.500000, 1740578.998000 5419812.500000, 1740578.998000 "
     "5419822.608000, 1740568.898000 5419822.608000, 1740568.898000 "
     "5419812.500000), (1740571.950000 5419815.555000, 1740575.945000 "
     "5419815.555000, 1740575.945000 5419819.552000, 1740571.950000 "
     "5419819.552000, 1740571.950000 "
-    "5419815.555000))\n36b1450b-f532-4a9b-9f6d-de52130cf741\tPolygon with "
-    "Multiple Holes\tRight\t769\t777\t8\t0\t18.4\t24\tPOLYGON "
+    "5419815.555000))\n36b1450b-f532-4a9b-9f6d-de52130cf741\t18950\tPolygon "
+    "with "
+    "Multiple Holes\tRight\t769\t777\t8\t0\t18.4\t24\tWood\tPOLYGON "
     "((1741756.244000 5420752.589000, 1741856.311000 5420752.589000, "
     "1741856.311000 5420852.676000, 1741756.244000 5420852.676000, "
     "1741756.244000 5420752.589000), (1741771.259000 5420772.604000, "
@@ -416,8 +424,9 @@ static constexpr char kWktCsv[] =
     "(1741786.274000 5420787.619000, 1741831.291000 5420787.619000, "
     "1741831.291000 5420817.648000, 1741786.274000 5420817.648000, "
     "1741786.274000 "
-    "5420787.619000))\n9d9a8926-958e-472c-b4a5-861912d6709d\tAOTEA "
-    "TCE\tRight\t475\t729\t254\t0\t381\t44\tMULTIPOLYGON (((1572223.199000 "
+    "5420787.619000))\n9d9a8926-958e-472c-b4a5-861912d6709d\t16529\tAOTEA "
+    "TCE\tRight\t475\t729\t254\t0\t381\t44\tAsphaltic concrete black "
+    "5\tMULTIPOLYGON (((1572223.199000 "
     "5176019.291000, 1572223.200000 5176019.376000, 1572223.161000 "
     "5176019.331000, 1572223.199000 5176019.291000)), ((1741756.244000 "
     "5420752.589000, 1741856.311000 5420752.589000, 1741856.311000 "
